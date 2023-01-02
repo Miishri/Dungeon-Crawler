a7cf47028b89c6dbe062897c9c1ecd304218e366
Miishri <muhamadshamaeem@gmail.com>
2023-01-02 08:01:44 +0100

    ADD TOSTRING

--- src/main/java/com/character/Skills.java
+++ src/main/java/com/character/Skills.java
@@ -10,6 +10,7 @@ public class Skills {
     private double percentTillLevel;
     private Rarity skillRarity;
 
+
     public Skills(String name, double damage, Rarity rarity) {
         skillName = name;
         skillLevel = 1;
@@ -21,5 +22,16 @@ public class Skills {
     //skill level up if double
     public void increaseSkillProgressLevel(double level){
         percentTillLevel += level;
+    }
+
+    //set cooldown based on rarity
+    @Override
+    public String toString(){
+        return  "NAME: " + skillName +
+                "| LVL: " + skillLevel +
+                "| DMG: " + skillDamage +
+                "| Proficiency: " + percentTillLevel +
+                "| RARITY: " + skillRarity
+                ;
     }
 }
