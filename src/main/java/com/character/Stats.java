a1f931dd530d37113d4ec81658ab363cd8b77d7d
Miishri <muhamadshamaeem@gmail.com>
2023-01-02 07:48:25 +0100

    HP, STAM, MAGIC, CH, DEX | ADD STAT GETTER/SETTER

--- src/main/java/com/character/Stats.java
+++ src/main/java/com/character/Stats.java
@@ -8,4 +8,61 @@ public class Stats {
     private double statCriticalHit;
     private int statDexterity;
 
+    public Stats(){}
+
+    //set hp
+    public void increaseStatHealth(int hp){
+        statHealth += hp;
+    }
+
+    //set stamina
+    public void increaseStatStamina(int stam) {
+        statStamina += stam;
+    }
+
+    //set magic
+    public void increaseStatMagic(int magic) {
+        statMagic += magic;
+    }
+
+    //set CH%
+    public void increaseStatCriticalHit(double ch) {
+        statCriticalHit += ch;
+    }
+
+    //set DEX
+    public void increaseStatDexterity(int dex) {
+        this.statDexterity += dex;
+    }
+
+    //GETTERS -----------------------------------------------------------------------------------------
+    public int getStatHealth() {
+        return statHealth;
+    }
+
+    public int getStatStamina() {
+        return statStamina;
+    }
+
+    public int getStatMagic() {
+        return statMagic;
+    }
+
+    public double getStatCriticalHit() {
+        return statCriticalHit;
+    }
+
+    public int getStatDexterity() {
+        return statDexterity;
+    }
+
+    @Override
+    public String toString() {
+        return  "HP: " + statHealth +
+                "| STAMINA: " + statStamina +
+                "| MAGIC: " + statMagic +
+                "| CRITICAL%: " + statCriticalHit +
+                "| DEX: " + statDexterity
+                ;
+    }
 }
