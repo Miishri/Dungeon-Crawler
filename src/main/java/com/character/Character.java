db85cd8ee95cfce53b8342905bac1e512c08e8e6
Miishri <muhamadshamaeem@gmail.com>
2023-01-02 06:39:26 +0100

    ADD EXTRA RPG CLASS, GET/SET-ers

--- src/main/java/com/character/Character.java
+++ src/main/java/com/character/Character.java
@@ -1,5 +1,36 @@
 package com.character;
 
+import java.util.ArrayList;
+
 public class Character {
+
+    private int characterLevel;
+    private final String characterName;
+    private double characterCoins;
+    private Class characterClass;
+    private ArrayList<Skills> characterSkills;
+
+    public Character(String name){
+        characterName = name;
+        characterLevel = 0;
+        characterCoins = 0.0;
+        characterClass = null;
+        characterSkills = new ArrayList<Skills>();
+    }
+
+    //increase level when boss dead or boost
+    public void increaseLevel(int addLevel){
+        this.characterLevel += addLevel;
+    }
+
+    //increase coins on randomDrop
+    public void increaseCoin(double coins){
+        this.characterCoins += coins;
+    }
+
+    //set string class on use or buy from shop
+    public void setCharacterClass(Class cClass){
+        characterClass = cClass;
+    }
 
 }
